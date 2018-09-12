7b01d580ce1d7585d57ac7530e59f046b611c9d3
Kyle Pollina <kylepollina@gmail.com>
2018-09-12 13:29:08 -0400

    v0.5 moved constants and variable definitions into a new file

---
+++ definitions.py
@@ -0,0 +1,42 @@
+# definitions.py
+#  definitions of constants for tetris game
+#  kyle pollina
+
+# Define constants
+WIDTH       = 20*20
+HEIGHT      = 20*30
+BOARDWIDTH  = 20*10
+BOARDHEIGHT = 20*20
+BOARDLEFT   = 5*20
+BOARDRIGHT  = 15*20
+BOARDTOP    = 5*20
+BOARDBOT    = 25*20
+FPS         = 30
+
+GAME_OVER = 0
+RUN = 1
+PAUSE = 2
+
+TESTSPEED   = 10
+NORMSPEED   = 20
+SLOWSPEED   = 30
+
+# Define colors
+WHITE  = (255,255,255)
+BLACK  = (0,0,0)
+RED    = (255,0,0)
+GREEN  = (0,255,0)
+BLUE   = (0,0,255)
+YELLOW = (255,255,0)
+CYAN   = (0,255,255)
+PURPLE = (255,0,255)
+ORANGE = (255,140,0)
+
+# Define block types
+SQUARE = 0
+LINE   = 1
+TBLOCK = 2
+ZBLOCK = 3
+SBLOCK = 4
+JBLOCK = 5
+LBLOCK = 6
