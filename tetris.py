d792a0cee811e7e0b90dacf0215624e5382c5b7a
Kyle Pollina <kylepollina@gmail.com>
2018-09-16 11:24:34 -0400

    v0.9 lots of new documentation. More modular

--- tetris.py
+++ tetris.py
@@ -6,28 +6,39 @@ import pygame
 import random
 from definitions import *
 
-# code to run the program
+##############################
+# run() - code to execute the game
+#  run() is called way down at the bottom of the file
+##############################
 def run():
     pygame_setup()
+    game_setup()
     game_loop()
     pygame.quit()
 
 
-# game loop
+##############################
+# game_loop() - code for running the game loop
+# 
+##############################
 def game_loop():
     global running
     running = True
 
     while running:
-        # keep loop running at the right speed
-        clock.tick(FPS)
-
+        # DO NOT EDIT
+        clock.tick(FPS)     # keeps the game running at the right speed
         process_input()
         # update
         draw()
 
+        
 
-# processes all user input
+
+##############################
+# process_input() - processes all user input
+#
+##############################
 def process_input():
     events = pygame.event.get()
     for event in events:
@@ -37,24 +48,42 @@ def process_input():
             running = False
             
 
-# draw / render
+
+##############################
+# draw() - draws objects on the screen. 
+#
+##############################
 def draw():
+    # DO NOT EDIT
+    pygame.display.flip()
+    
+    # EDIT HERE
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, (BOARDLEFT - 2, BOARDTOP - 2, BOARDWIDTH + 4, BOARDHEIGHT + 4), 1)
     pygame.draw.rect(screen, WHITE, (BOARDRIGHT + 18, BOARDTOP - 2, 74, 94), 1)
-    all_sprites.draw(screen) 
     render_text('NEXT', WHITE, BLACK, BOARDRIGHT + 35, BOARDTOP - 20)
-
-    # After drawing everything, flip the display
-    pygame.display.flip()
+    all_sprites.draw(screen) 
 
 
-# setting up pygame environment
+
+##############################
+# game_setup() - setting up the actual game
+#
+##############################
+def game_setup():
+    global all_sprites  # list of all sprites currently existing
+
+    all_sprites = pygame.sprite.Group()
+
+
+##############################
+# pygame_setup() - setting up the pygame environment
+#  probably shouldn't change anything here other than font
+##############################
 def pygame_setup():
-    global screen
-    global font
-    global clock
-    global all_sprites
+    global screen       # screen object of size WIDTH x HEIGHT
+    global font         # font type
+    global clock        # game clock
 
     pygame.init()
     pygame.mixer.init()
@@ -64,10 +93,16 @@ def pygame_setup():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     font = pygame.font.SysFont(None, 20)
     clock = pygame.time.Clock()
-    all_sprites = pygame.sprite.Group()
 
  
-# render text on the screen
+##############################
+# render_text() - render text on the screen
+#  string  - the string of the text you wish to render
+#  color   - the color of the font
+#  bgcolor - the background color of the text
+#  x - x coordinate
+#  y - y coordinate
+##############################
 def render_text(string, color, bgcolor, x, y):
     textsurface = font.render(string, True, color, bgcolor)
     screen.blit(textsurface, (x,y))
@@ -78,3 +113,5 @@ def render_text(string, color, bgcolor, x, y):
 # Start program
 ##############################
 run()
+
+
